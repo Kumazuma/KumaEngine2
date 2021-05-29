8cfe0b4091574ffb8121741540ac5ecf038bfc6d
Damezuma <qweads12@gmail.com>
2021-05-29 20:29:11 +0900

    Impl Device Initialization

--- Kuma.Render.D3D11/Renderer.cpp
+++ Kuma.Render.D3D11/Renderer.cpp
@@ -1,10 +1,13 @@
 #include"pch.h"
 #include"Renderer.h"
 #include<functional>
+#include<array>
 namespace KumaEngine::cpp
 {
 	D3D11RenderModule::D3D11RenderModule():
-		isRunning_{ false }
+		isRunning_{ false },
+		currentBackBufferIndex{ 0 },
+		fenceValue_{ 0 }
 	{
 
 	}
@@ -16,18 +19,104 @@ namespace KumaEngine::cpp
 			renderThread_.join();
 		}
 		weakRef_->Unlink();
+		CloseHandle(hFenceEvent_);
 	}
 	IFACEMETHODIMP D3D11RenderModule::Initialize(HWND hWnd, const GameRenderDesc* desc)
 	{
+		HRESULT hr = S_OK;
+		DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+		swapChainDesc.Width = desc->width;
+		swapChainDesc.Height = desc->height;
+		swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		swapChainDesc.BufferCount = SWAP_CHAIN_COUNT;
+		swapChainDesc.Scaling = DXGI_SCALING_ASPECT_RATIO_STRETCH;
+		swapChainDesc.Stereo = false;
+		swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+		swapChainDesc.SampleDesc.Count = 0;
+		swapChainDesc.SampleDesc.Quality = 0;
+		swapChainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER | DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_UNORDERED_ACCESS;
+		swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_FULLSCREEN_VIDEO;
+		DXGI_SWAP_CHAIN_FULLSCREEN_DESC fullScreenDesc{};
+		if (desc->fullscreen)
+		{
+			fullScreenDesc.RefreshRate.Numerator = 0;
+			fullScreenDesc.RefreshRate.Denominator = 1;
+			fullScreenDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+			fullScreenDesc.Windowed = false;
+			fullScreenDesc.Scaling = DXGI_MODE_SCALING_STRETCHED;
+		}
+		ComPtr<IDXGIFactory5> dxgiFactory;
+		if (FAILED(hr = CreateDXGIFactory2(0, __uuidof(IDXGIFactory5), &dxgiFactory)))
+		{
+			return E_FAIL;
+		}
+		D3D_FEATURE_LEVEL feature_level = D3D_FEATURE_LEVEL_11_1;
+
+		ComPtr<ID3D11Device> device;
+		ComPtr<ID3D11DeviceContext> context;
+		hr = D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, D3D11_CREATE_DEVICE_DEBUG, &feature_level, 1, D3D11_SDK_VERSION, &device, &feature_level, &context);
+		if (FAILED(hr))
+		{
+			return E_FAIL;
+		}
+		
+		hr = device.As(&device_);
+		if (FAILED(hr))
+		{
+			return E_FAIL;
+		}
+		
+		hr = context.As(&deviceContext_);
+		if (FAILED(hr))
+		{
+			return E_FAIL;
+		}
+
+		ComPtr<IDXGISwapChain1> swapChain;
+
+		hr = dxgiFactory->CreateSwapChainForHwnd(device_.Get(), hWnd, &swapChainDesc, desc->fullscreen ? &fullScreenDesc : nullptr, nullptr, &swapChain);
+		if (FAILED(hr))
+		{
+			return E_FAIL;
+		}
+
+		hr = swapChain.As(&swapChain_);
+		if (FAILED(hr))
+		{
+			return E_FAIL;
+		}
+		
+		swapChain_->GetBuffer(0, __uuidof(ID3D11Texture2D), &swapChainTextures[0]);
+		swapChain_->GetBuffer(1, __uuidof(ID3D11Texture2D), &swapChainTextures[1]);
+		device_->CreateRenderTargetView(swapChainTextures[0].Get(), nullptr, &swapChainViews[0]);
+		device_->CreateRenderTargetView(swapChainTextures[1].Get(), nullptr, &swapChainViews[1]);
+		device_->CreateFence(0, D3D11_FENCE_FLAG_NONE, __uuidof(fence_), &fence_);
+		hFenceEvent_ = CreateEventW(nullptr, false, false, nullptr);
 		renderThread_ = std::thread(&D3D11RenderModule::Process, this);
-		return E_NOTIMPL;
+		return S_OK;
 	}
 	
 	void D3D11RenderModule::Process()
 	{
 		while (isRunning_)
 		{
+			if (mainCamera_ == nullptr)
+			{
+				SwitchToThread();
+				continue;
+			}
+			deviceContext_->ClearRenderTargetView(swapChainViews[0].Get(), std::array<float, 4>({ 0.f, 0.f, 1.f, 1.f }).data());
 
+			swapChain_->Present(1, 0);
+			const UINT64 fence = fenceValue_;
+			++fenceValue_;
+			// Wait until the previous frame is finished.
+			if (fence_->GetCompletedValue() < fence)
+			{
+				fence_->SetEventOnCompletion(fence, hFenceEvent_);
+				WaitForSingleObject(hFenceEvent_, INFINITE);
+			}
+			currentBackBufferIndex = swapChain_->GetCurrentBackBufferIndex();
 		}
 	}
 
--- Kuma.Render.D3D11/Renderer.h
+++ Kuma.Render.D3D11/Renderer.h
@@ -1,36 +1,44 @@
 #pragma once
 #include "KumaEngine2.h"
 #include "helper.h"
-#include <d3d11.h>
+#include<d3d11_4.h>
+#include<dxgi1_5.h>
 #include <wrl.h>
 #include <thread>
 namespace KumaEngine::cpp
 {
 	using namespace Microsoft::WRL;
-
+	constexpr int SWAP_CHAIN_COUNT{ 2 };
 	class D3D11RenderModule: public RefCountImpl<IRenderModule, IEntity, IUnknown>
 	{
 	public:
 		D3D11RenderModule();
 		~D3D11RenderModule();
-		STDMETHOD(Initialize(HWND hWnd, const ::GameRenderDesc* desc));
-		STDMETHOD(GetWeakRef(IWeakRef** ref));
-		STDMETHOD(Update());
-		STDMETHOD(CreateMeshRenderer(IMeshRenderer** meshRenderer));
-		STDMETHOD(CreateCamera(ICamera** camera));
-		STDMETHOD(LoadMeshFromFile(const wchar_t* meshId, const wchar_t* filePath));
-		STDMETHOD(LoadMeshFromMemory(const wchar_t* meshId, const wchar_t* ext, const uint8_t* bytes, size_t byteLength));
-		STDMETHOD(LoadTextureFromFile(const wchar_t* textureId, const wchar_t* filePath));
-		STDMETHOD(LoadTextureFromMemory(const wchar_t* textureId, const wchar_t* ext, const uint8_t* bytes, size_t byteLength));
-		STDMETHOD(SetMainCamera(ICamera* camera));
-
+		IFACEMETHODIMP Initialize(HWND hWnd, const ::GameRenderDesc* desc);
+		IFACEMETHODIMP GetWeakRef(IWeakRef** ref);
+		IFACEMETHODIMP Update();
+		IFACEMETHODIMP CreateMeshRenderer(IMeshRenderer** meshRenderer);
+		IFACEMETHODIMP CreateCamera(ICamera** camera);
+		IFACEMETHODIMP LoadMeshFromFile(const wchar_t* meshId, const wchar_t* filePath);
+		IFACEMETHODIMP LoadMeshFromMemory(const wchar_t* meshId, const wchar_t* ext, const uint8_t* bytes, size_t byteLength);
+		IFACEMETHODIMP LoadTextureFromFile(const wchar_t* textureId, const wchar_t* filePath);
+		IFACEMETHODIMP LoadTextureFromMemory(const wchar_t* textureId, const wchar_t* ext, const uint8_t* bytes, size_t byteLength);
+		IFACEMETHODIMP SetMainCamera(ICamera* camera);
 	private:
 		void Process();
 	private:
 		bool isRunning_;
 		ComPtr<CommonWeakRef> weakRef_;
-		ComPtr<ID3D11Device> device_;
-		ComPtr<ID3D11DeviceContext> deviceContext_;
+		ComPtr<ID3D11Device5> device_;
+		ComPtr<ID3D11DeviceContext4> deviceContext_;
+		ComPtr<IDXGISwapChain3> swapChain_;
+		ComPtr<ICamera> mainCamera_;
+		ComPtr<ID3D11RenderTargetView> swapChainViews[SWAP_CHAIN_COUNT];
+		ComPtr<ID3D11Texture2D> swapChainTextures[SWAP_CHAIN_COUNT];
+		ComPtr<ID3D11Fence> fence_;
+		HANDLE hFenceEvent_;
+		UINT64 fenceValue_;
+		INT currentBackBufferIndex;
 		std::thread renderThread_;
 	};
 }
